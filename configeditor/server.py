a44729648796eb0b5687f6f5f4687e867b2b98e5
cxs <chengxiaosan@gmail.com>
2023-07-04 18:10:58 +0800

    config editor

---
+++ configeditor/server.py
@@ -0,0 +1,178 @@
+from enum import Enum
+from pathlib import Path
+import json
+
+import streamlit as st
+import streamlit_pydantic as sp
+from pydantic import BaseModel, Field
+
+
+class Speaker(str, Enum):
+    mute = "mute"
+    zhy = "zhy"
+
+
+class BotModel(BaseModel):
+    id: str = Field(
+        ...,
+        title="id",
+        description="一旦建立，不可修改",
+        readOnly=True
+    )
+    name: str = Field(
+        "storyteller",
+        title="名称",
+        description="展示给玩家的昵称"
+    )
+    description: str = Field(
+        "bot",
+        title="描述",
+        description="可能展示给玩家"
+    )
+    speaker: Speaker = Field(
+        Speaker.zhy,
+        title="音色",
+        description="语音选择,mute:无音色，zhy:曾海月"
+    )
+    greeting: str = Field(
+        "你好",
+        title="问好",
+        description="bot主动说的第一句话",
+        format="multi-line"
+    )
+    prompt: str = Field(
+        "I want you to act as a storyteller. You will come up with entertaining stories that are engaging, "
+        "imaginative and captivating for the audience.",
+        title="提示词",
+        description="注意token数规划，gpt-3.5是2048，格式为S:[system message]\nU:[user message]\nA:[assistant message]",
+        format="multi-line",
+        st_kwargs_height=300
+    )
+
+    info: str = Field(
+        "",
+        title="策划注释",
+        description="策划自用，随便填",
+        format="multi-line"
+    )
+
+    def to_profile_json(self):
+        return self.json(exclude={'id', 'prompt', 'info'}, indent=4, ensure_ascii=False)
+
+    def save_bot(self, bots_dir: Path):
+        this_bot_dir = bots_dir / self.id
+        if not this_bot_dir.exists():
+            this_bot_dir.mkdir()
+
+        (this_bot_dir / 'profile.json').write_text(self.to_profile_json(), encoding="utf8")
+        if self.prompt:
+            (this_bot_dir / 'prompt.txt').write_text(self.prompt, encoding="utf8")
+        if self.info:
+            (this_bot_dir / 'info.txt').write_text(self.info, encoding="utf8")
+
+    @staticmethod
+    def load_bot(this_bot_dir: Path):
+        fp = (this_bot_dir / 'profile.json')
+        if not fp.exists():
+            return None
+        profile = fp.read_text(encoding='utf8')
+
+        fp = (this_bot_dir / 'prompt.txt')
+        prompt = fp.read_text(encoding='utf8') if fp.exists() else ""
+        fp = (this_bot_dir / 'info.txt')
+        info = fp.read_text(encoding='utf8') if fp.exists() else ""
+
+        kwargs = json.loads(profile)
+        kwargs['id'] = this_bot_dir.name
+        kwargs['prompt'] = prompt
+        kwargs['info'] = info
+
+        bot = BotModel.parse_obj(kwargs)
+        return bot
+
+    @staticmethod
+    def delete_bot(delete_bot_dir: Path):
+        (delete_bot_dir / 'profile.json').unlink()
+        (delete_bot_dir / 'prompt.txt').unlink(missing_ok=True)
+        (delete_bot_dir / 'info.txt').unlink(missing_ok=True)
+        delete_bot_dir.rmdir()
+
+
+class BotConfigEditor:
+    def __init__(self, config: Path):
+        self.bots_dir = config / 'bots'
+        if 'bots' not in st.session_state:
+            st.session_state.bots = {}
+            for b in self.bots_dir.iterdir():
+                if b.is_dir():
+                    bot = BotModel.load_bot(b)
+                    if bot:
+                        st.session_state.bots[bot.id] = bot
+
+        if 'cur' not in st.session_state:
+            if len(st.session_state.bots) > 0:
+                for bot in st.session_state.bots.values():
+                    st.session_state.cur = bot
+                    break
+
+    def show(self):
+        with st.sidebar:
+            st.divider()
+            st.header("bots")
+            for bot_id, bot in st.session_state.bots.items():
+                if st.button(bot_id):
+                    st.session_state.cur = bot
+
+            st.divider()
+            with st.expander("new bot"):
+                with st.form("new"):
+                    new_bot_id = st.text_input("new bot id")
+                    if st.form_submit_button("create"):
+                        if len(new_bot_id) == 0:
+                            st.error("名称为空，无法创建")
+                        elif new_bot_id in st.session_state.bots:
+                            st.error(f"{new_bot_id}重名，无法创建")
+                        else:
+                            st.session_state.cur = BotModel(id=new_bot_id)
+
+            with st.expander("delete bot"):
+                with st.form("delete"):
+                    delete_bot_id = st.text_input("delete bot id")
+                    delete_sure = st.text_input("are you sure", placeholder="yes! delete", help="输入yes! delete确认")
+                    if st.form_submit_button("delete"):
+                        if len(delete_bot_id) == 0:
+                            st.error("名称为空，无法删除")
+                        elif delete_bot_id not in st.session_state.bots:
+                            st.error(f"{delete_bot_id}不在bots列表中，无法删除")
+                        elif delete_sure != "yes! delete":
+                            st.error("确认code输入不对，请输入yes! delete")
+                        else:
+                            self.delete_bot(delete_bot_id)
+                            if 'cur' in st.session_state and st.session_state.cur.id == delete_bot_id:
+                                del st.session_state['cur']
+                            st.experimental_rerun()
+
+        if 'cur' in st.session_state:
+            cur = st.session_state.cur
+            is_update = cur.id in st.session_state.bots
+            intent = f"更新{cur.id}" if is_update else f"创建{cur.id}"
+            st.header(intent)
+            bot = sp.pydantic_form(key=cur.id, model=cur, submit_label=intent)
+            if bot:
+                # st.json(bot.to_profile_json())
+                self.save_bot(bot)
+                if not is_update:
+                    st.experimental_rerun()
+
+    def save_bot(self, bot: BotModel):
+        bot.save_bot(self.bots_dir)
+        st.session_state.bots[bot.id] = bot
+
+    def delete_bot(self, delete_bot_id: str):
+        BotModel.delete_bot(self.bots_dir / delete_bot_id)
+        del st.session_state.bots[delete_bot_id]
+
+
+if __name__ == '__main__':
+    editor = BotConfigEditor(Path('../configdir'))
+    editor.show()
